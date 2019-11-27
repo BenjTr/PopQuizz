49dc6e5db4675e1f1171c369344817a2e7803669
Benjamin TROTIN <benjamin.trotin@gmail.com>
2019-11-27 16:40:19 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GameComponent } from './game/game.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    HomeComponent,
+    ConfirmationComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
