03dd9c64d153f5e19a46798bf74621062944f1f6
Nicolas Bolo <nicoco.bolo@gmail.com>
2020-01-04 14:57:34 +0100

    Working login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GameComponent } from './game/game.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UserComponent } from './user/user.component';
-import { HomeComponent } from './home/home.component';
-import { ConfirmationComponent } from './confirmation/confirmation.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {GameComponent} from './game/game.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {UserComponent} from './user/user.component';
+import {HomeComponent} from './home/home.component';
+import {ConfirmationComponent} from './confirmation/confirmation.component';
+import {ChangePasswordComponent} from './change-password/change-password.component';
 import {HttpClientModule} from '@angular/common/http';
 
-import {
-  MatInputModule,
-  MatButtonModule,
- } from '@angular/material';
+import {MatButtonModule, MatInputModule} from '@angular/material';
+import {StorageServiceModule} from 'angular-webstorage-service';
+import {UtilsService} from "./utils/utils.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GameComponent,
-    LoginComponent,
-    RegisterComponent,
-    UserComponent,
-    HomeComponent,
-    ConfirmationComponent,
-    ChangePasswordComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    MatButtonModule,
-    MatInputModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        GameComponent,
+        LoginComponent,
+        RegisterComponent,
+        UserComponent,
+        HomeComponent,
+        ConfirmationComponent,
+        ChangePasswordComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatButtonModule,
+        MatInputModule,
+        HttpClientModule,
+        StorageServiceModule
+    ],
+    providers: [UtilsService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
